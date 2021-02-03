99701786c579d58365f8cf7d49d4a8fb1f095f0d
migamador <miguel.amador@yahoo.com>
2021-02-02 22:58:08 -0500

    Api and Mvc  MediatR

--- CleanArch.Api/Controllers/CourseController.cs
+++ CleanArch.Api/Controllers/CourseController.cs
@@ -4,6 +4,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
 
 namespace CleanArch.Api.Controllers
 {
@@ -11,6 +13,18 @@ namespace CleanArch.Api.Controllers
     [Route("[controller]")]
     public class CourseController : ControllerBase
     {
-       
+        private readonly ICourseService _courseService;
+
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] CourseViewModel courseViewModel)
+        {
+            _courseService.Create(courseViewModel);
+            return Ok(courseViewModel);
+        }
     }
 }
